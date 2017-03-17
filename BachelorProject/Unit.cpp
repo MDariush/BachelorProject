9cfb6b88c1875062f925480e574a81b8f69f138f
MDariush <dariush@live.dk>
2017-03-17 17:15:15 +0900

    Added unit

---
+++ BachelorProject/Unit.cpp
@@ -0,0 +1,17 @@
+/*
+Unit.cpp
+Created by Martin Dariush Hansen, 2017-03-17
+*/
+
+#include "Unit.h"
+
+Unit::Unit() {
+}
+
+Unit::~Unit() {
+}
+
+void Unit::Init(long double x0, long double y0) {
+	x = x0;
+	y = y0;
+}
---
+++ BachelorProject/Unit.h
@@ -0,0 +1,23 @@
+/*
+Unit.h
+Created by Martin Dariush Hansen, 2017-03-17
+*/
+
+#pragma once
+
+class Unit
+{
+public:
+	Unit();
+	~Unit();
+	void Init(long double x0, long double y0);
+
+private:
+	long double x, y;
+	long double direction;
+	long double orientation;
+	long double orientationAcc;
+	long double spdAcc;
+	long double maxSpd;
+	unsigned int visionRng;
+};
